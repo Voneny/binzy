2f70aba662ffee4c9f1e83bae187e20cb2063203
voneny <steaminging@seznam.cz>
2025-02-19 17:36:00 +0100

    druhej commit

--- app/page.js
+++ app/page.js
@@ -1,53 +1,55 @@
-import Image from "next/image";
+'use client';
+
+import React from 'react';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
+    <div className="min-h-screen w-full bg-gray-100 text-gray-900 flex flex-col">
+      <header className="bg-blue-500 text-white p-4 w-full"> 
+        <h1 className="text-3xl font-bold text-left m-2">Půjčovna kol</h1>
+      </header>
+      <div className='flex-grow p-4 bg-white shadow-lg rounded-lg flex flex-col items-start gap-4 sm:flex-row'>
+        <main className="flex-grow p-4 bg-white shadow-lg rounded-lg flex flex-col md:flex-row items-start gap-4 sm:flex-row-reverse">
+          <div className='md:w-2/3 '>
+            <h2 className='text-2xl font-bold'>Vše o cykloturistice na Třeboňsku</h2>
+              <p className='text-gray-600'>
+              Tyto stránky jsou věnovány všem příznivcům cyklistiky, zvláště pak cykloturistům 
+              a milovníkům krásné přírody v krajině třeboňských rybníků. Najdete zde informace 
+              o možnostech ubytování, oblíbené cyklistické trasy a mnoho dalších rad a doporučení.
+              </p>
+          </div>
+          <div className='md:w-1/3 '>
+            <img src='https://www.itrebon.cz/images/preview/1613562853.jpg' alt='Cykloturistika' className='w-full mt-4 rounded-lg shadow-lg'/>
+          </div>
+        </main>
+        <aside className='md:w-1/3'>
+          <div className="card bg-primary text-primary-content w-96">
+            <div className="card-body">
+              <h2 className="card-title">Doporučujeme</h2>
+              <p>Na kole okolo Třeboně: z Chlumu u Třeboně přes obec Kosky, po hrázi rybníků Nový a Starý Kanclíř do Lutové, okolo Mokřin přes Stříbřec a Stříbřecký most po hrázích rybníků Stolec, Vyšehrad, Nový a Starý Vdovec do Staré Hliny, ze Staré do Nové a okolo Obecního mlýna do Třeboně, v Třeboni z hráze rybníka Svět k Opatovickému rybníku, dále ke Zlaté stoce a přes Barboru a Majdalenu zpět do Chlumu.</p>
+              <div className="card-actions justify-end">
+                <button className="btn">Více...</button>
+              </div>
+            </div>
+          </div>
+          <div className="card bg-primary text-primary-content w-96">
+            <div className="card-body">
+              <h2 className="card-title">Půjčovna kol</h2>
+              <p>•   Dětská kola od 12" do 20"
+               <br/> •   Treková a horská kola 24" až 29"
+               <br/> •   Elektrokola Standard
+               <br/> •   Elektrokola Vyšší třída
+               <br/> •   Elektrokola Top
+               <br/> •   Koloběžky
+               <br/> •   Dětské vozíky
+               <br/> •   Dětské sedačky (pouze se zapůjčeným kolem)</p>
+              <div className="card-actions justify-end">
+                <button className="btn">Více...</button>
+              </div>
+            </div>
+          </div>
+        </aside>
+      </div>
     </div>
+  );
 }
---
+++ componet/Counter.js
@@ -0,0 +1,5 @@
+function Counter(props) {
+  return (<div>{props.title}</div>);
+}
+
+export default Counter;
